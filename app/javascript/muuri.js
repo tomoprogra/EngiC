0bbb2b318d4040965dd577c87d18c529d17d11a5
tomoprogra <anyonewww@outlook.jp>
2024-02-06 13:39:54 +0900

    bullet導入

--- app/javascript/muuri.js
+++ app/javascript/muuri.js
@@ -1,25 +1,28 @@
 import Muuri from 'muuri';
 
-var grid = new Muuri('.grid', {
-  dragEnabled: true,
-  layoutOnInit: false,
-  sortData: {
-    id: function (item, element) {
-      return parseFloat(element.children[0].textContent);
+document.addEventListener('turbo:load', function () {
+  var grid = new Muuri('.grid', {
+    dragEnabled: true,
+    layoutOnInit: false,
+    sortData: {
+      id: function (item, element) {
+        return parseFloat(element.children[0].textContent);
+      }
     }
-  }
-});
-var sortAsc = document.querySelector('.sort-asc');
-var sortDesc = document.querySelector('.sort-desc');
+  });
 
-// Sort the items before the initial layout
-// and do the initial layout
-grid.sort('id', {layout: 'instant'})
+  var sortAsc = document.querySelector('.sort-asc');
+  var sortDesc = document.querySelector('.sort-desc');
 
-// Bind the button event listeners
-sortAsc.addEventListener('click', function () {
-  grid.sort('id');
-});
-sortDesc.addEventListener('click', function () {
-  grid.sort('id:desc');
+  // Sort the items before the initial layout
+  // and do the initial layout
+  grid.sort('id', {layout: 'instant'});
+
+  // Bind the button event listeners
+  sortAsc.addEventListener('click', function () {
+    grid.sort('id');
+  });
+  sortDesc.addEventListener('click', function () {
+    grid.sort('id:desc');
+  });
 });
