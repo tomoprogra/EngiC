388fc7bf52d6caf94539f22d7ad6f374f3a83c33
tomoprogra <anyonewww@outlook.jp>
2024-02-08 10:44:20 +0900

    ローカルでのschema.rbの変更をコミット

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_05_071921) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_013827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "mypage_id"
+    t.index ["mypage_id"], name: "index_items_on_mypage_id"
+  end
+
+  create_table "mypages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_mypages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,8 +40,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_05_071921) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.text "introduction"
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "mypages", "users"
 end
