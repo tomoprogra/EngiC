4a701cebe6d868b4e93a5b9ba286227f862b2ad7
tomoprogra <anyonewww@outlook.jp>
2024-02-14 18:07:46 +0900

    flashmessage実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook, :twitter, :github]
+         :omniauthable, omniauth_providers: [:twitter, :github]
   has_one :mypage, dependent: :destroy
   has_many :identities, dependent: :destroy
   # ユーザーがフォローしている人
