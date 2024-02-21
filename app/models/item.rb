d83c220bc977caccec43cd6da326af88f4355ab8
tomoprogra <anyonewww@outlook.jp>
2024-02-21 13:35:39 +0900

    rubocop

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   validates :xname, uniqueness: true, allow_nil: true
   validates :githubname, uniqueness: true, allow_nil: true
   mount_uploader :imageurl, ImageUrlUploader
+  validates :title, length: {maximum:60}
 end
